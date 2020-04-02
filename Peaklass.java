1162fb40de5782a2de5076941d364d9f556d6f4e
marietempel <62176360+marietempel@users.noreply.github.com>
2020-04-02 12:51:37 +0300

    Update Peaklass.java

--- Peaklass.java
+++ Peaklass.java
@@ -10,37 +10,33 @@ import java.util.List;
 import java.util.Scanner;
 
 import static rühmatöö1.Raamat.*;
+import static rühmatöö1.Raamaturiiul.*;
 
 public class Peaklass {
 
     public static void main(String[] args) throws Exception {
 
-        //Ma ei suuda mõelda hetkel, missugune see fail välja peaks nägema või kuidas sealt andmeid võtma peaks
-        //java.io.File fail = new java.io.File("C:/Users/tempel/Documents/OOP/oop/src/nadal4/kodu/marsruut.txt");
-        // sinu fail on ilmselt teise teekonnaga jätame mõlemad sisse, lihtsalt igaüks kommenteerib välja selle, mis
-        // talle ei sobi.
-        //File fail = new File("C:\\Users\\carmen akkermann\\IdeaProjects\\OOP2\\src\\rühmatöö1\\raamatud.txt");
+
+        java.io.File failRaamatud = new java.io.File("C:/Users/tempel/Documents/OOP/oop/src/rühmatöö1/raamatud");
+        java.io.File failRiiulid = new java.io.File("C:/Users/tempel/Documents/OOP/oop/src/rühmatöö1/riiulid.txt");
+        //File failRaamatud = new File("C:\\Users\\carmen akkermann\\IdeaProjects\\OOP2\\src\\rühmatöö1\\raamatud.txt");
         //String fail = "raamatud.txt";
 
-        Raamaturiiul sinine = new Raamaturiiul(1,2,"sinine");
-        Raamaturiiul suurTuba = new Raamaturiiul(7,5, "Suur Tuba");
-        Raamaturiiul marieTuba = new Raamaturiiul(9,4, "Marie tuba");
-        //Raamat minaOlenSurm = new Raamat("Mina Olen Surm", "Mairi Laurik", 224, suurTuba, new Riiul(3,4));
-        //System.out.println(minaOlenSurm);
+        Raamaturiiul sinine = new Raamaturiiul("sinine", 1, 2);
 
-        List<String[]> massiivideList = loeRaamatudFailist(fail);
-        //kontrolliks
-        /*for (String[] strings : massiivideList) {
-            for (int i = 0; i < 11; i++) {
-                System.out.print(strings[i] + " ");
+        int soovitudRiiuliIndeks =0; // idee, kuidas otsida välja ainult mingi kindla soovitud tunnusega raamatud
+        riiuliIsenditeLoomineJaListiLisamine(failRiiulid);
+        for (Raamaturiiul riiul : kõigiRaamaturiiuliteList) {
+            if (riiul.getAsukohtVõiTunnusmärk().equals("sinine")){ //leiab soovitud riiuli
+                soovitudRiiuliIndeks = kõigiRaamaturiiuliteList.indexOf(riiul);
             }
-            System.out.println();
-        }*/
-
+        }
+        List<String[]> massiivideList = loeRaamatudFailist(failRaamatud);
         isenditeLoomineJaListiLisamine(massiivideList);
-
-        for (Raamat raamat : kõigiRaamatuteList) {
-            System.out.println(raamat);
+        for (Raamat raamat : kõigiRaamatuteList) { // väljastab ainult sinise riiuli raamatud hetkel
+            if (raamat.getTäpneAsukoht().getRaamaturiiul().equals(kõigiRaamaturiiuliteList.get(soovitudRiiuliIndeks))) {
+                System.out.println(raamat);
+            }
         }
     }
 }
